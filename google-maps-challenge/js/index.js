d98bacd46e7cbc611b4a357e93247e2ff41a0e26
Fernando Ochoa <56469077+ferochoavi@users.noreply.github.com>
2020-04-10 10:07:07 -0500

    More bugs fixed

--- google-maps-challenge/js/index.js
+++ google-maps-challenge/js/index.js
@@ -47,15 +47,17 @@ function displayStores(){
 }
 
 function showStoresMarkers(){
-
+    var bounds = new google.maps.LatLngBounds();
     for(var [index, store] of stores.entries()){
         var latlng = new google.maps.LatLng(
             store["coordinates"]["latitude"],
             store["coordinates"]["longitude"]);
         var name = store["name"];
         var address = store["addressLines"][0];
+        bounds.extend(latlng);
         createMarker(latlng, name, address, index+1)
     }
+    map.fitBounds(bounds);
 }
 
 function createMarker(latlng, name, address, index){
