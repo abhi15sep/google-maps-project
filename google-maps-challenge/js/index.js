1a4b9110a5b38008ca9ad88a4db50ce74491fd19
Fernando Ochoa <56469077+ferochoavi@users.noreply.github.com>
2020-04-10 08:32:50 -0500

    great update

---
+++ google-maps-challenge/js/index.js
@@ -0,0 +1,71 @@
+//console.log("HELLO WORLD"); The JS print in console command
+
+//When you load the website I want this to initiate
+window.onload = () => { displayStores(); } //the syntax before siumulates a function in ES6
+
+var map;
+var markers = [];
+var infoWindow;
+
+function initMap() {
+    var LosAngeles = {
+        lat: 34.063380, 
+        lng: -118.358080
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: LosAngeles,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+    });
+    showStoresMarkers();
+}
+
+function displayStores(){
+    var storesHtml = '';
+    for(var [index, store] of stores.entries()){
+        var address = store['addressLines'];
+        var phone = store['phoneNumber']
+        
+        storesHtml += `
+            <div class="stores-list">
+                <div class="store-container">
+                    <div class="store-info-container">
+                        <div class="store-address">
+                            <span>${address[0]}</span>
+                            <span>${address[1]}</span>
+                        </div>
+                        <div class="store-phone-number">${phone}</div>
+                    </div>
+                    <div class="store-number-container">
+                        <div class="store-number">${index + 1}</div>
+                    </div>
+                </div>
+            </div>
+        `
+        document.querySelector('.stores-list').innerHTML = storesHtml;
+    }
+}
+
+function showStoresMarkers(){
+
+    for(var [index, store] of stores.entries()){
+        
+        var latlng = new google.maps.LatLng(
+            store["coordinates"]["latitude"],
+            store["coordinates"]["longitude"]
+        );
+        var name = store["name"];
+        var address = store["addessLines"][0];
+        createMarker(latlng, name, address, index+1)
+    }
+}
+
+function createMarker(latlng, name, address, index){
+    var html = "<b>" + name + "</b> <br/>" + address;
+    var marker = new google.maps.Marker({map: map, position: latlng});
+    /*google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent(html);
+        infoWindow.open(map, marker);
+    }); */
+    markers.push(marker);
+}
--- google-maps-challenge/todo.md
+++ google-maps-challenge/todo.md
@@ -1,2 +1,23 @@
 # Plan Of Action
 
+- Create Wireframe DONE
+
+- Set Up de base HTML structure DONE
+
+- Import Google Maps into window DONE
+
+- Add the text "Store Locator" DONE
+
+- Add input box DONE
+
+- Add Store List Container DONE
+
+- Add and style the individual store container DONE
+
+- Show all the stores in the store's list based on real world data DONE (with exception, check out later)
+
+- Show the marker based on real world store data
+
+- Show the info window when you click on the individual store
+
+- Add a beautiful transition on the hover of the individual store
